14a9629be2aa62c915be7f2aa75b05e75e46bcfe
Yury Pliashkou <pliashkou@gmail.com>
2019-05-28 22:55:25 +0300

    Init

---
+++ ios/Pods/Target Support Files/Pods-mshop-tvOS/Pods-mshop-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mshop_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_mshop_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-mshopTests/Pods-mshopTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mshopTests : NSObject
+@end
+@implementation PodsDummy_Pods_mshopTests
+@end
